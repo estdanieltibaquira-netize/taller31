c35104ee44440c3b73d12af19e8ad68c8243b146
estdanieltibaquira-netize <est.daniel.tibaquira@unimilitar.edu.co>
2026-05-08 12:56:31 -0500

    Implementa algoritmo de recorte de líneas Cohen-Sutherland y reorganizar funciones

--- script.js
+++ script.js
@@ -1,23 +1,12 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-
-ctx.fillStyle = "red";
-ctx.fillRect(50, 50, 10, 10);
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-
-// cambiar sistema de coordenadas
+// sistema de coordenadas
 ctx.translate(0, canvas.height);
 ctx.scale(1, -1);
 
-
-ctx.fillStyle = "red";
-ctx.fillRect(50, 50, 10, 10);
-
-// dibuja linea pixel a pixel
+// dibuja linea (Bresenham)
 function drawLine(x0, y0, x1, y1) {
-
     let dx = Math.abs(x1 - x0);
     let dy = Math.abs(y1 - y0);
 
@@ -27,7 +16,7 @@ function drawLine(x0, y0, x1, y1) {
     let err = dx - dy;
 
     while (true) {
-        ctx.fillRect(x0, y0, 1, 1); // dibuja pixel
+        ctx.fillRect(x0, y0, 1, 1);
 
         if (x0 === x1 && y0 === y1) break;
 
@@ -44,39 +33,86 @@ function drawLine(x0, y0, x1, y1) {
         }
     }
 }
-// dibuja rectangulo de recorte
-function drawViewport(xmin, ymin, xmax, ymax) {
+// recorta linea
+function cohenSutherland(x0, y0, x1, y1, xmin, ymin, xmax, ymax) {
 
+    let code0 = getCode(x0, y0, xmin, ymin, xmax, ymax);
+    let code1 = getCode(x1, y1, xmin, ymin, xmax, ymax);
+
+    let accept = false;
+
+    while (true) {
+
+        if ((code0 | code1) === 0) {
+            accept = true;
+            break;
+        } 
+        else if (code0 & code1) {
+            break;
+        } 
+        else {
+
+            let x, y;
+            let codeOut = code0 ? code0 : code1;
+
+            if (codeOut & TOP) {
+                x = x0 + (x1 - x0) * (ymax - y0) / (y1 - y0);
+                y = ymax;
+            } 
+            else if (codeOut & BOTTOM) {
+                x = x0 + (x1 - x0) * (ymin - y0) / (y1 - y0);
+                y = ymin;
+            } 
+            else if (codeOut & RIGHT) {
+                y = y0 + (y1 - y0) * (xmax - x0) / (x1 - x0);
+                x = xmax;
+            } 
+            else if (codeOut & LEFT) {
+                y = y0 + (y1 - y0) * (xmin - x0) / (x1 - x0);
+                x = xmin;
+            }
+
+            if (codeOut === code0) {
+                x0 = x;
+                y0 = y;
+                code0 = getCode(x0, y0, xmin, ymin, xmax, ymax);
+            } else {
+                x1 = x;
+                y1 = y;
+                code1 = getCode(x1, y1, xmin, ymin, xmax, ymax);
+            }
+        }
+    }
+
+    if (accept) {
+        drawLine(x0, y0, x1, y1);
+    }
+}
+// viewport
+function drawViewport(xmin, ymin, xmax, ymax) {
     drawLine(xmin, ymin, xmax, ymin);
     drawLine(xmax, ymin, xmax, ymax);
     drawLine(xmax, ymax, xmin, ymax);
     drawLine(xmin, ymax, xmin, ymin);
 }
-// lineas de prueba (5 casos)
+
+// datos
 let lineas = [
-    {x0: 100, y0: 100, x1: 200, y1: 200}, // dentro
-    {x0: -50, y0: -50, x1: -10, y1: -10}, // fuera
-    {x0: 50, y0: 50, x1: 300, y1: 300},   // cruza
-    {x0: 300, y0: 50, x1: 50, y1: 300},   // diagonal
-    {x0: 150, y0: -50, x1: 150, y1: 300}  // vertical
+    {x0: 100, y0: 100, x1: 200, y1: 200},
+    {x0: -50, y0: -50, x1: -10, y1: -10},
+    {x0: 50, y0: 50, x1: 300, y1: 300},
+    {x0: 300, y0: 50, x1: 50, y1: 300},
+    {x0: 150, y0: -50, x1: 150, y1: 300}
 ];
-// codigos binarios
-const INSIDE = 0;
-const LEFT = 1;
-const RIGHT = 2;
-const BOTTOM = 4;
-const TOP = 8;
 
-// calcula region del punto
-function getCode(x, y, xmin, ymin, xmax, ymax) {
+// ventana
+let xmin = 100, ymin = 100, xmax = 300, ymax = 250;
 
-    let code = INSIDE;
+// dibujar
+ctx.fillStyle = "blue";
+drawViewport(xmin, ymin, xmax, ymax);
 
-    if (x < xmin) code |= LEFT;
-    else if (x > xmax) code |= RIGHT;
-
-    if (y < ymin) code |= BOTTOM;
-    else if (y > ymax) code |= TOP;
-
-    return code;
+ctx.fillStyle = "red";
+for (let l of lineas) {
+    cohenSutherland(l.x0, l.y0, l.x1, l.y1, xmin, ymin, xmax, ymax);
 }
