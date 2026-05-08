bff12c36f02cc33935c9703bd39569e874ac366d
estdanieltibaquira-netize <est.daniel.tibaquira@unimilitar.edu.co>
2026-05-08 12:27:25 -0500

    Implementamos función personalizada para trazar líneas con Bresenham

--- script.js
+++ script.js
@@ -14,3 +14,33 @@ ctx.scale(1, -1);
 
 ctx.fillStyle = "red";
 ctx.fillRect(50, 50, 10, 10);
+
+// dibuja linea pixel a pixel
+function drawLine(x0, y0, x1, y1) {
+
+    let dx = Math.abs(x1 - x0);
+    let dy = Math.abs(y1 - y0);
+
+    let sx = (x0 < x1) ? 1 : -1;
+    let sy = (y0 < y1) ? 1 : -1;
+
+    let err = dx - dy;
+
+    while (true) {
+        ctx.fillRect(x0, y0, 1, 1); // dibuja pixel
+
+        if (x0 === x1 && y0 === y1) break;
+
+        let e2 = 2 * err;
+
+        if (e2 > -dy) {
+            err -= dy;
+            x0 += sx;
+        }
+
+        if (e2 < dx) {
+            err += dx;
+            y0 += sy;
+        }
+    }
+}
