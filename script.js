f934752cdfe32e640e06ef3d01f8754565b84d44
nikoll-12 <est.nikoll.borja@unimilitar.edu.co>
2026-05-08 12:15:28 -0500

    Se configura el sistema de coordenadas con origen en la esquina inferior izquierda

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-// prueba simple
+
+ctx.fillStyle = "red";
+ctx.fillRect(50, 50, 10, 10);
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+// cambiar sistema de coordenadas
+ctx.translate(0, canvas.height);
+ctx.scale(1, -1);
+
+
 ctx.fillStyle = "red";
 ctx.fillRect(50, 50, 10, 10);
